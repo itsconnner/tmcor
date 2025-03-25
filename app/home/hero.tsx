95dd16bd04779b03c426f44b64ea9bad756bc1e4
Jiamu Sun <barroit@linux.com>
2025-03-26 04:43:27 +0900

    hero: add masking to video
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- app/home/hero.tsx
+++ app/home/hero.tsx
@@ -1,43 +1,73 @@
 import { useEffect, useRef } from "react";
 import useAnimationFrame from "~/hooks/useAnimationFrame";
 
-
 export default function Hero() {
-    const cur = useRef({ x: 0, y: 0 });
-    const bg : any = useRef(null);
+    const cursor = useRef({ x: 0, y: 0 });
+    const offset = useRef({ x: 0, y: 0 });
+    const video : any = useRef(null);
+    const mask : any = useRef(null);
+    const container : any = useRef(null);
 
     useAnimationFrame(() => {
-        const x = cur.current.x * 0.5;
-        const y = cur.current.y * 0.5;
+        offset.current.x += (cursor.current.x - offset.current.x) * 0.1;
+        offset.current.y += (cursor.current.y - offset.current.y) * 0.1;
 
-        bg.current.style.transform = `translate3d(${x}px, ${y}px, 0px)`;
+        const x = offset.current.x;
+        const y = offset.current.y;
+
+        video.current.style.transform = `translate3d(${x}px, ${y}px, 0)`;
     });
 
     function sync_cur_pos(e: MouseEvent) {
-        cur.current.x = -(e.clientX / window.innerWidth - 0.5) * 270;
-        cur.current.y = -(e.clientY / window.innerHeight - 0.5) * 270;
+        const pos = container.current.getBoundingClientRect();
+
+        if (e.clientY > pos.bottom)
+            return;
+
+        const x = -(e.clientX / window.innerWidth - 0.5);
+        const y = -(e.clientY / window.innerHeight - 0.5);
+
+        cursor.current.x = Math.min(220, x * 220);
+        cursor.current.y = Math.min(110, y * 110);
+    }
+
+    function fade_video()
+    {
+        const pos = mask.current.getBoundingClientRect();
+        const center = window.innerHeight;
+
+        const off = center - pos.bottom;
+        const height = center + pos.height;
+
+        const y = Math.max(Math.min(off / height, 1), 0);
+
+        mask.current.style.setProperty('--scroll-opacity', y * 2);
     }
 
     useEffect(() => {
         window.addEventListener('mousemove', sync_cur_pos);
+        window.addEventListener('scroll', fade_video);
 
-        return () => window.removeEventListener('mousemove', sync_cur_pos);
+        return () => {
+            window.removeEventListener('mousemove', sync_cur_pos);
+            window.removeEventListener('scroll', fade_video);
+        }
     }, []);
 
-    return (
-    <section className='relative h-screen w-screen'>
-        <div className='absolute left-0 top-0 h-screen w-screen overflow-hidden'>
-            <video ref={bg} src="hero.mp4"
-                   className='min-h-[calc(100%+220px)] min-w-[calc(100%+520px)]
-                              absolute -top-[110px] -left-[260px] object-cover'
-                   autoPlay muted loop playsInline/>
-        </div>
-        {/* <div className='sticky h-lvh w-full overflow-clip'>
-            <video ref={bg} src="https://redbarrelsgames.com/wp-content/uploads/2024/05/outlast_maison_test_V03_2.5k.mp4"
-                   className='absolute object-cover
-                              h-[calc(100%+560px)] w-[calc(100%+540px)]
-                              -top-[110px]' width={2560} height={1440} muted loop playsInline disableRemotePlayback />
-        </div> */}
-
-    </section>);
+return (
+  <section className='h-screen w-screen touch-pan-y'>
+    <div ref={container} className='h-full w-full bg-black overflow-hidden'>
+      <div ref={mask} style={{ '--scroll-opacity': 0 }}
+           className='absolute bottom-0 h-full w-full z-1 bg-black
+                      opacity-(--scroll-opacity) duration-300 ease-linear'/>
+      <div className='relative h-[calc(100%+220px)]
+                      w-[calc(100%+520px)] bg-black'>
+        <video ref={video} src="hero.mp4"
+               className='absolute w-full h-full
+                          -top-[110px] -left-[260px] object-cover'
+               muted loop playsInline/>
+      </div>
+    </div>
+  </section>
+);
 }
