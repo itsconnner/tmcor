fb9a8292dcc474ae0401a4224cbbbe072571268e
Jiamu Sun <barroit@linux.com>
2025-04-02 15:35:00 +0900

    TMP 3
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- app/header.tsx
+++ app/header.tsx
@@ -1,19 +1,28 @@
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import heroFilter from '~/home/Filter';
+import { CSSTransition } from 'react-transition-group';
+import '~/header.css';
 
 export default function Header() {
     const [open, set_open] = useState(0);
+    const menu = useRef(null);
+    const mask = useRef(null);
 
     return (
         <header className={ heroFilter('fixed w-full touch-pan-y pt-4 px-6 z-39',
             'text-2xl font-medium **:uppercase',
             open && 'touch-none') }>
 
-            <div className={ heroFilter('absolute xl:hidden left-0 top-0 h-screen w-screen',
-                'transition-color duration-400',
-                open && 'touch-none bg-[#020409]/80 h-full') }
-            />
+            <CSSTransition in={open}
+                           nodeRef={mask}
+                           timeout={400}
+                           classNames='mask'
+                           unmountOnExit>
+                <div ref={mask}
+                     className='absolute left-0 top-0 h-screen
+                                touch-none w-screen transition-color duration-400'/>
+	    </CSSTransition>
 
             <div className='relative flex justify-between'>
 
@@ -42,26 +51,30 @@ export default function Header() {
 
                 </div>
 
-                <ul className={ heroFilter('absolute xl:hidden left-0 top-full',
-                    'w-full h-screen mt-15',
-                    '*:w-full *:py-5 text-3xl',
-                    '*:flex *:flex-col *:justify-between',
-                    '*:transition-all *:duration-200',
-                    open ? '*:opacity-100 *:translate-y-0' : '*:opacity-0 *:-translate-y-10') }>
+                <CSSTransition in={open}
+                               nodeRef={menu}
+                               timeout={400}
+                               classNames='menu'
+                               unmountOnExit>
+                    <ul ref={menu}
+                        className='absolute left-0 top-full w-full h-screen
+                                   mt-15 *:w-full *:py-5 text-3xl *:flex *:flex-col
+                                   *:justify-between *:transition-all *:opacity-0
+                                   *:-translate-y-10 *:duration-200'>
 
-                    <li>
-                        <button>• about</button>
-                    </li>
+                        <li>
+                            <button>• about</button>
+                        </li>
 
-                    <li className='delay-100'>
-                        <button>• [ EN ] /CN</button>
-                    </li>
+                        <li className='delay-100'>
+                            <button>• [ EN ] /CN</button>
+                        </li>
 
-                    <li className='delay-200'>
-                        <button>• pre-order now</button>
-                    </li>
-
-                </ul>
+                        <li className='delay-200'>
+                            <button>• pre-order now</button>
+                        </li>
+                    </ul>
+                </CSSTransition>
             </div>
         </header>);
 }
--- app/home/intro.tsx
+++ app/home/intro.tsx
@@ -2,14 +2,14 @@
 export default function Intro()
 {
     return (
-        <section className='container mx-auto xl:h-screen min-h-screen w-full'>
+        <section className='container mx-auto xl:h-screen min-h-screen mt-10 w-full'>
 
             <div className='h-full flex flex-col
                             xl:flex-row justify-center items-center gap-y-10
                             md:gap-y-20 gap-x-20'>
 
                 <div className='relative w-1/2 xl:w-full flex justify-center
-                                mt-20 mb-10 xl:my-0 pointer-events-none select-none'>
+                                my-10 xl:my-0 pointer-events-none select-none'>
 
                     <div className='absolute bottom-0 -right-2/7
                                     xl:right-0 w-3/5 xl:w-1/3 z-1'>
